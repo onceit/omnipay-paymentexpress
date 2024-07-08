d87e45a825a057a8a3cf4c6d5a35fb5dd41f127a
Youfa <youfa@onceit.co.nz>
2024-07-09 09:51:19 +1200

    Remove capture

--- src/PxPayGateway.php
+++ src/PxPayGateway.php
@@ -107,13 +107,13 @@ class PxPayGateway extends AbstractGateway
         return $this->completeAuthorize($parameters);
     }
 
-    public function capture(array $parameters = array())
-    {
-        return $this->createRequest('\Omnipay\PaymentExpress\Message\PxPayCaptureRequest', $parameters);
-    }
+    // public function capture(array $parameters = array())
+    // {
+    //     return $this->createRequest('\Omnipay\PaymentExpress\Message\PxPayCaptureRequest', $parameters);
+    // }
 
-    public function completeCapture(array $parameters = array())
-    {
-        return $this->completeAuthorize($parameters);
-    }
+    // public function completeCapture(array $parameters = array())
+    // {
+    //     return $this->completeAuthorize($parameters);
+    // }
 }
