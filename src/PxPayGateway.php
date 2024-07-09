57d5592128f5c904d5e8518eb1d3927ee72ebf7d
Youfa <youfa@onceit.co.nz>
2024-07-09 12:13:21 +1200

    Update version

--- src/PxPayGateway.php
+++ src/PxPayGateway.php
@@ -16,7 +16,7 @@ class PxPayGateway extends AbstractGateway
 {
     public function getName()
     {
-        return 'PaymentExpress PxPay';
+        return 'PaymentExpress PxPay1';
     }
 
     public function getDefaultParameters()
@@ -107,10 +107,10 @@ class PxPayGateway extends AbstractGateway
         return $this->completeAuthorize($parameters);
     }
 
-    // public function capture(array $parameters = array())
-    // {
-    //     return $this->createRequest('\Omnipay\PaymentExpress\Message\PxPayCaptureRequest', $parameters);
-    // }
+    public function capture(array $parameters = array())
+    {
+        return $this->createRequest('\Omnipay\PaymentExpress\Message\PxPayCaptureRequest', $parameters);
+    }
 
     // public function completeCapture(array $parameters = array())
     // {
